b5e05fb1b83928ea910da5488b90324eaa3602b6
vicsystems <victorasuquob@gmail.com>
2025-02-03 15:14:45 +0100

    first commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,209 @@
+<script setup>
+import heroBg from '@/assets/img/33450.jpg';
+
+const backgroundStyle = {
+  backgroundImage: `url(${heroBg})`,
+  backgroundSize: 'cover',
+  backgroundPosition: 'center center',
+};
+</script>
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav class="fixed w-full bg-black shadow-lg p-4 z-20">
+        <div class="container mx-auto flex justify-between items-center">
+            <div class="flex items-center space-x-3">
+                <img src="../assets/img/logo.png" alt="YG Logo" class="h-10">
+                <h1 class="text-lg font-bold text-gold">YG Movie Productions</h1>
+            </div>
+            <div class="hidden md:flex space-x-6"> <a href="#hero" class="hover:text-gold">Home</a>
+                <a href="#about" class="hover:text-gold">About Us</a>
+                <a href="#works" class="hover:text-gold">Our Works</a>
+                <a href="#join" class="hover:text-gold">Get Started</a>
+            </div>
+            <div class="md:hidden"> <button id="menuBtn" class="text-gold">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M4 6h16M4 12h16M4 18h16"></path>
+                    </svg>
+                </button>
+            </div>
+        </div>
+
+        <div id="mobileMenu" class="hidden fixed top-0 left-0 w-full h-full bg-black/80 z-30">
+            <div class="bg-black w-3/4 h-full p-6">
+                <button id="closeMenuBtn" class="text-gold absolute top-4 right-4">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12">
+                        </path>
+                    </svg>
+                </button>
+                <ul class="flex flex-col space-y-4 mt-12">
+                    <li><a href="#hero" class="text-white hover:text-gold">Home</a></li>
+                    <li><a href="#about" class="text-white hover:text-gold">About Us</a></li>
+                    <li><a href="#works" class="text-white hover:text-gold">Our Works</a></li>
+                    <li><a href="#join" class="text-white hover:text-gold">Get Started</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+
+
+<section id="hero" class="h-screen flex items-center justify-center text-center relative"
+     :style="backgroundStyle"
+    >
+    <div class="absolute inset-0 bg-gradient-to-b from-black/90 to-black/50"></div>
+    <div class="relative z-10 px-4">
+        <h2 class="text-5xl sm:text-8xl font-bold text-gold/90 drop-shadow-lg leading-tight">Join the Future of Filmmaking</h2>
+        <div class="max-w-xl mx-auto">
+            <p class="mt-5 py-5 text-white/90 drop-shadow-md text-base sm:text-xl">
+                Young Gold seeks passionate content creators for collaboration! Gain exposure, industry connections, and exclusive opportunities with YG Movie Productions. Partner with us and showcase your talent—sign up today!
+            </p>
+        </div>
+        <a href="#join"
+            class="mt-6 inline-block bg-orange-500 text-black px-8 py-4 font-bold rounded-lg text-base sm:text-xl hover:bg-orange-600 transition duration-300">
+            Get Started
+        </a>
+    </div>
+</section>
+
+
+    <section id="about" class="py-20 px-6 bg-black">
+        <div class="max-w-7xl mx-auto flex flex-col md:flex-row items-center">
+            <div class="md:w-1/2 p-6">
+                <img src="../assets/img/about.jpg" alt="About Us Image" class="rounded-lg shadow-lg">
+            </div>
+
+            <div class="md:w-1/2 p-6 text-left text-white">
+                <h3 class="text-3xl font-bold">Who We Are</h3>
+                <p class="mt-4">YG Movie Productions focuses on movie production, talent development, and creative
+                    storytelling.</p>
+
+                <h4 class="mt-8 text-2xl font-bold">Mission</h4>
+                <p class="mt-2">YG Movie Productions is committed to entertaining, inspiring, and sparking intellectual
+                    discourse by creating a deep understanding of the human condition across cultures through the
+                    powerful medium of cinematic storytelling.</p>
+
+                <h4 class="mt-8 text-2xl font-bold">Vision</h4>
+                <p class="mt-2">To serve as a mirror reflecting the rich tapestry of cultural diversity across the
+                    global society. We envision a future where the stories brought to life on the silver screen emanate
+                    from the authentic voices of storytellers from all backgrounds.</p>
+
+                <h4 class="mt-8 text-2xl font-bold">Commitment</h4>
+                <p class="mt-2">We encourage young and upcoming talents who are passionate about making a positive
+                    influence in society and driven to take Nollywood to the next level.</p>
+
+
+            </div>
+        </div>
+        <div class="max-w-4xl mx-auto mt-8 text-center">
+            <h4 class="text-2xl font-bold text-white py-5">Our Standards</h4>
+            <div class="mt-2 flex flex-wrap justify-center space-x-4 md:space-x-8">
+                <div class="px-4 py-2 bg-orange-500 rounded-md text-white font-bold text-xl m-2">Talent</div>
+                <div class="px-4 py-2 bg-orange-500 rounded-md text-white font-bold text-xl m-2">Integrity</div>
+                <div class="px-4 py-2 bg-orange-500 rounded-md text-white font-bold text-xl m-2">Passion</div>
+                <div class="px-4 py-2 bg-orange-500 rounded-md text-white font-bold text-xl m-2">Unbiased</div>
+                <div class="px-4 py-2 bg-orange-500 rounded-md text-white font-bold text-xl m-2">Realistic</div>
+            </div>
+        </div>
+
+    </section>
+
+    <section id="join" class="py-20 px-6 bg-gold text-black">
+        <div class="max-w-2xl mx-auto text-center">
+            <h3 class="text-3xl font-bold">Join Us Now</h3>
+            <form class="mt-8 space-y-4">
+                <div>
+                    <label for="fullName" class="block text-left font-medium">Full Name <span class="text-red-500">*</span></label>
+                    <input type="text" id="fullName" name="fullName" placeholder="Full Name" class="w-full p-3 border rounded mt-1" required>
+                </div>
+                <div>
+                    <label for="email" class="block text-left font-medium">Email <span class="text-red-500">*</span></label>
+                    <input type="email" id="email" name="email" placeholder="Email" class="w-full p-3 border rounded mt-1" required>
+                </div>
+                <div>
+                    <label for="phone" class="block text-left font-medium">Phone Number <span class="text-red-500">*</span></label>
+                    <input type="tel" id="phone" name="phone" placeholder="Phone Number" class="w-full p-3 border rounded mt-1" required>
+                </div>
+                <div>
+                    <label for="address" class="block text-left font-medium">Address <span class="text-red-500">*</span></label>
+                    <input type="text" id="address" name="address" placeholder="Address" class="w-full p-3 border rounded mt-1" required>
+                </div>
+    
+                <div>
+                    <label for="facebook" class="block text-left font-medium">Facebook Profile URL</label>
+                    <input type="url" id="facebook" name="facebook" placeholder="Facebook URL" class="w-full p-3 border rounded mt-1">
+                </div>
+                <div>
+                    <label for="instagram" class="block text-left font-medium">Instagram Profile URL</label>
+                    <input type="url" id="instagram" name="instagram" placeholder="Instagram URL" class="w-full p-3 border rounded mt-1">
+                </div>
+                <div>
+                    <label for="tiktok" class="block text-left font-medium">TikTok Profile URL</label>
+                    <input type="url" id="tiktok" name="tiktok" placeholder="TikTok URL" class="w-full p-3 border rounded mt-1">
+                </div>
+                <div>
+                    <label for="twitter" class="block text-left font-medium">Twitter Profile URL</label>
+                    <input type="url" id="twitter" name="twitter" placeholder="Twitter URL" class="w-full p-3 border rounded mt-1">
+                </div>
+                <div>
+                    <label for="other" class="block text-left font-medium">Other Social Media/Portfolio Link</label>
+                    <input type="url" id="other" name="other" placeholder="Other Link" class="w-full p-3 border rounded mt-1">
+                </div>
+    
+                <input type="file" class="w-full p-3 border rounded">
+                <button class="w-full bg-black text-white py-3 font-bold rounded">Submit</button>
+            </form>
+        </div>
+    </section>
+
+    <!-- Our Works Section -->
+    <section id="works" class="py-20 px-6">
+        <div class="max-w-4xl mx-auto ">
+            <h3 class="text-3xl font-bold text-gold text-center">Our Works</h3>
+            <div class="mt-8 grid grid-cols-1 md:grid-cols-2 gap-6">
+                <div>
+                    <a href="https://www.youtube.com/watch?v=RaD0MAxMZRU">
+                        <img src="../assets/img/cover1.png" class="w-full rounded">
+                        <p class="mt-2">THE PA</p>
+                    </a>
+                </div>
+                <div>
+                    <a href="https://www.youtube.com/watch?v=Vbd6GU7u0gI">
+                        <img src="../assets/img/cover2.png" class="w-full rounded">
+                        <p class="mt-2">LETHAL AFFECTION</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Footer -->
+    <footer class="bg-black text-center py-8">
+        <div>
+            <h3 class="text-xl font-bold text-gold">YG Movie Productions</h3>
+            <p class="mt-2">Contact: info@ygmovies.erp-55.com | Phone: +123456789</p>
+            <div class="mt-4 space-x-4">
+                <a href="https://www.facebook.com/profile.php?id=61559220190668" class="text-gold"><i class="fab fa-facebook"></i></a>
+                <a href="https://www.instagram.com/younggold_mp/" class="text-gold"><i class="fab fa-instagram"></i></a>
+                <a href="https://www.youtube.com/@YOUNGGOLDMOVIEPRODUCTION" class="text-gold"><i class="fab fa-youtube"></i></a>
+            </div>
+        </div>
+    </footer>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+
   }
 }
 </script>
